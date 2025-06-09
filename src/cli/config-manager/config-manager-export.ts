225040ac138433ba6752628b0da70c60a2f4e6ce
Devin Holderness <dholderness@trivir.com>
2025-06-09 11:37:16 -0600

    wip

--- src/cli/config-manager/config-manager-export.ts
+++ src/cli/config-manager/config-manager-export.ts
@@ -1,5 +1,6 @@
 import { FrodoStubCommand } from '../FrodoCommand';
 import Terms from './config-manager-export-terms-and-conditions';
+import Themes from './config-manager-export-themes';
 
 export default function setup() {
   const program = new FrodoStubCommand('config-manager export').description(
@@ -7,6 +8,8 @@ export default function setup() {
   );
 
   program.addCommand(Terms().name('terms-and-conditions'));
+  program.addCommand(Themes().name('themes'));
+
 
   return program;
 }
