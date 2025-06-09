36e4d4e4dc26f25f316e597f81ed9cf421d49082
Devin Holderness <dholderness@trivir.com>
2025-06-09 11:39:43 -0600

    wip

---
+++ src/cli/config-manager/config-manager-export-themes.ts
@@ -0,0 +1,44 @@
+import { exportFrConfigThemesToFiles } from '../../configManagerOps/FrConfigThemeOps';
+import { getTokens } from '../../ops/AuthenticateOps';
+import { printMessage, verboseMessage } from '../../utils/Console';
+import { FrodoCommand } from '../FrodoCommand';
+
+const deploymentTypes = ['cloud', 'forgeops'];
+
+export default function setup() {
+  const program = new FrodoCommand(
+    'frodo config-manager export themes',
+    [],
+    deploymentTypes
+  );
+
+  program
+    .description('Export themes.')
+    .action(async (host, realm, user, password, options, command) => {
+      command.handleDefaultArgsAndOpts(
+        host,
+        realm,
+        user,
+        password,
+        options,
+        command
+      );
+
+      if (await getTokens(false, true, deploymentTypes)) {
+        verboseMessage('Exporting themes');
+        const outcome = await exportFrConfigThemesToFiles();
+        if (!outcome) process.exitCode = 1;
+      }
+      // unrecognized combination of options or no options
+      else {
+        printMessage(
+          'Unrecognized combination of options or no options...',
+          'error'
+        );
+        program.help();
+        process.exitCode = 1;
+      }
+    });
+
+  return program;
+}
