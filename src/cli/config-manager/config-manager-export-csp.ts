7424aa7416bbe2407af9c4a1520724ec0f0882a7
mricketttrivir <mrickett@trivir.com>
2025-06-13 12:00:42 -0600

    Updated csp command to use config file

--- src/cli/config-manager/config-manager-export-csp.ts
+++ src/cli/config-manager/config-manager-export-csp.ts
@@ -1,3 +1,5 @@
+import { Option } from 'commander';
+
 import { exportCsp } from '../../configManagerOps/FrConfigCspOps';
 import { getTokens } from '../../ops/AuthenticateOps';
 import { printMessage, verboseMessage } from '../../utils/Console';
@@ -14,6 +16,30 @@ export default function setup() {
 
   program
     .description('Export content security policy.')
+    /** 
+     * added because fr-config manager has an option to use a config file for the "fr-config-pull csp" command. Bryan said this should still be supported
+     * 
+     * -----------------------  Example CSP_OVERRIDES json file ----------------------------------
+    {
+      "enforced": {
+        "active": {
+          "$bool": "${CSP_ENFORCED}"
+        }
+      },
+      "report-only": {
+        "active": {
+          "$bool": "${CSP_REPORT_ONLY}"
+        }
+      }
+    }
+     * -------------------------------------------------------------------------------------------- 
+     */
+    .addOption(
+      new Option(
+        '-f, --file <file>',
+        'The CSP_OVERRIDES json file. ex: "/home/trivir/Documents/csp-overrides.json", or "csp-overrides.json"'
+      )
+    )
     .action(async (host, realm, user, password, options, command) => {
       command.handleDefaultArgsAndOpts(
         host,
@@ -26,7 +52,7 @@ export default function setup() {
 
       if (await getTokens(false, true, deploymentTypes)) {
         verboseMessage('Exporting content security policy');
-        const outcome = await exportCsp();
+        const outcome = await exportCsp(options.file);
         if (!outcome) process.exitCode = 1;
       }
       // unrecognized combination of options or no options
