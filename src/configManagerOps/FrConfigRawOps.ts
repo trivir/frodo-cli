4bf9a151cc925a4816fcf18e29e3082142b1c794
mricketttrivir <mrickett@trivir.com>
2025-08-06 13:59:23 -0600

    Removed direct RawApi calls

--- src/configManagerOps/FrConfigRawOps.ts
+++ src/configManagerOps/FrConfigRawOps.ts
@@ -5,7 +5,7 @@ import { readFile } from 'fs/promises';
 import { printError, verboseMessage } from '../utils/Console';
 
 const { getFilePath, saveJsonToFile, json } = frodo.utils;
-const { exportRawIdm, exportRawAm, exportRawEnv } = frodo.raw;
+const { exportRawConfig } = frodo.rawConfig;
 
 /**
  * Export every item from the list in the provided json file
@@ -17,45 +17,7 @@ export async function configManagerExportRaw(file: string): Promise<boolean> {
 
     // Create export json file for every item in the provided json file
     for (const config of jsonData) {
-      let response: IdObjectSkeletonInterface;
-
-      // remove starting slash from path if it exists
-      if (config.path.startsWith('/')) {
-        config.path = config.path.substring(1);
-      }
-
-      // support for only three root paths, am, openidm, and environment
-      const urlParts: string[] = config.path.split('/');
-      const startPath: string = urlParts.reverse().pop();
-      const noStart: string = urlParts.reverse().join('/');
-      switch (startPath) {
-        case 'openidm':
-          response = await exportRawIdm(noStart);
-          break;
-        case 'am':
-          response = await exportRawAm(noStart);
-          // fr-config-manager has this option, only for am end points
-          if (config.pushApiVersion) {
-            response._pushApiVersion = config.pushApiVersion;
-          }
-          break;
-        case 'environment':
-          response = await exportRawEnv(noStart);
-          break;
-        default:
-          printError(
-            new Error(
-              `URL paths that start with ${startPath} are not supported`
-            )
-          );
-          break;
-      }
-
-      // all endpoints can have overrides
-      if (config.overrides) {
-        response = json.mergeDeep(response, config.overrides);
-      }
-
+      let response: IdObjectSkeletonInterface = await exportRawConfig(config);
       verboseMessage(`Saving ${response._id} at ${config.path}.json.`);
       saveJsonToFile(
         response,
