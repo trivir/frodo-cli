90cf5a97dcb602ed470c7a6a88b0af4ca9229f3a
Devin Holderness <dholderness@trivir.com>
2025-11-11 10:33:33 -0700

    log-api keys

--- src/cli/log/log-key-save.ts
+++ src/cli/log/log-key-save.ts
@@ -1,7 +1,9 @@
-import { frodo } from '@rockcarver/frodo-lib';
+import { frodo, state } from '@rockcarver/frodo-lib';
 import { Option } from 'commander';
 
 import { getTokens } from '../../ops/AuthenticateOps';
+import { updateLogApiKey } from '../../ops/LogOps';
+import { verboseMessage } from '../../utils/Console';
 import { FrodoCommand } from '../FrodoCommand';
 
 const { CLOUD_DEPLOYMENT_TYPE_KEY } = frodo.utils.constants;
@@ -10,19 +12,20 @@ const deploymentTypes = [CLOUD_DEPLOYMENT_TYPE_KEY];
 
 export default function setup() {
   const program = new FrodoCommand(
-    'frodo log key describe',
+    'frodo log key save',
     ['realm'],
     deploymentTypes
   );
 
   program
-    .description('Describe log API keys.')
+    .description('Save an existing API key to a connection profile.')
     .addOption(
-      new Option(
-        '-i, --key-id <key-id>',
-        'Key id. If specified, -a and -A are ignored.'
-      )
+      new Option('-i, --key-id <id>', 'ID of the API key.').makeOptionMandatory()
     )
+    .addOption(
+        new Option('-s, --key-secret <secret>', 'Secret of the API key.').makeOptionMandatory()
+    )
+
     .action(
       // implement command logic inside action handler
       async (host, user, password, options, command) => {
@@ -33,8 +36,10 @@ export default function setup() {
           options,
           command
         );
-        if (await getTokens(false, true, deploymentTypes)) {
-          // code goes here
+        if (await getTokens(true, true, deploymentTypes)) {
+          verboseMessage(`Saving log API key to connection profile ${host}...`);
+          const outcome = await updateLogApiKey(options.keyId, options.keySecret);
+          if (!outcome) process.exitCode = 1;
         } else {
           process.exitCode = 1;
         }
