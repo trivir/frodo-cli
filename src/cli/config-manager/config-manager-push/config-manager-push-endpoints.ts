1a7f4b246b2f3394483507a41850736940d48952
Dallin Sevy <dsevy@trivir.com>
2026-03-09 12:15:55 -0600

    feat: add config-manager push endpoints command

---
+++ src/cli/config-manager/config-manager-push/config-manager-push-endpoints.ts
@@ -0,0 +1,52 @@
+import { Option } from 'commander';
+
+import { configManagerImportEndpoints } from '../../../configManagerOps/FrConfigEndpointsOps';
+import { getTokens } from '../../../ops/AuthenticateOps';
+import { printMessage, verboseMessage } from '../../../utils/Console';
+import { FrodoCommand } from '../../FrodoCommand';
+
+const deploymentTypes = ['cloud', 'forgeops'];
+
+export default function setup() {
+  const program = new FrodoCommand(
+    'frodo config-manager push endpoints',
+    [],
+    deploymentTypes
+  );
+
+  program
+    .description('Import custom endpoints objects.')
+    .addOption(
+      new Option(
+        '-n, --name <name>',
+        'Endpoint name, import only specified enpoint'
+      )
+    )
+    .action(async (host, realm, user, password, options, command) => {
+      command.handleDefaultArgsAndOpts(
+        host,
+        realm,
+        user,
+        password,
+        options,
+        command
+      );
+
+      if (await getTokens(false, true, deploymentTypes)) {
+        verboseMessage('Importing config entity endpoints');
+        const outcome = await configManagerImportEndpoints(options.name);
+        if (!outcome) process.exitCode = 1;
+      }
+      // unrecognized combination of options or no options
+      else {
+        printMessage(
+          'Unrecognized combination of options or no options...',
+          'error'
+        );
+        program.help();
+        process.exitCode = 1;
+      }
+    });
+
+  return program;
+}
