764fe8f8886c1d1269f17bb98e6252da618d6572
Dallin Sevy <dsevy@trivir.com>
2026-03-11 16:11:04 -0600

    remove unecessary directories. Remove -r realm option

--- src/cli/config-manager/config-manager-push/config-manager-push-org-privileges.ts
+++ src/cli/config-manager/config-manager-push/config-manager-push-org-privileges.ts
@@ -2,10 +2,8 @@ import { frodo } from '@rockcarver/frodo-lib';
 import { Option } from 'commander';
 
 import {
-  configManagerImportOrgPrivilegeAssignments,
   configManagerImportOrgPrivilegesAllRealms,
   configManagerImportOrgPrivilegesByName,
-  configManagerImportOrgPrivilegesRealm,
 } from '../../../configManagerOps/FrConfigOrgPrivilegesOps';
 import { getTokens } from '../../../ops/AuthenticateOps';
 import { printMessage } from '../../../utils/Console';
@@ -18,7 +16,6 @@ const deploymentTypes = [
   CLOUD_DEPLOYMENT_TYPE_KEY,
   FORGEOPS_DEPLOYMENT_TYPE_KEY,
 ];
-const { constants } = frodo.utils;
 
 export default function setup() {
   const program = new FrodoCommand(
@@ -47,26 +44,17 @@ export default function setup() {
 
       if (await getTokens(false, true, deploymentTypes)) {
         let outcome: boolean;
-
         if (options.name) {
           printMessage(
             `Importing organization privileges config for "${options.name}"`
           );
           outcome = await configManagerImportOrgPrivilegesByName(options.name);
-        } else if (realm !== constants.DEFAULT_REALM_KEY) {
-          printMessage(
-            `Importing organization privileges config to the realm: "${realm}"`
-          );
-          outcome =
-            (await configManagerImportOrgPrivilegeAssignments()) &&
-            (await configManagerImportOrgPrivilegesRealm(realm));
         } else {
           printMessage(
             'Importing organization privileges config to all realms'
           );
           outcome = await configManagerImportOrgPrivilegesAllRealms();
         }
-
         if (!outcome) process.exitCode = 1;
       }
       // unrecognized combination of options or no options
