f0a1cde34205ede7d15bf7457f3bf4a2acc26e12
Dallin Sevy <dsevy@trivir.com>
2026-04-21 10:28:04 -0600

    feat: Add frodo config-manager push custom-nodes command

--- src/configManagerOps/FrConfigCustomNodesOps.ts
+++ src/configManagerOps/FrConfigCustomNodesOps.ts
@@ -1,9 +1,10 @@
 import { frodo } from '@rockcarver/frodo-lib';
+import fs from 'fs';
 
 import { printError } from '../utils/Console';
 
 const { saveJsonToFile, getFilePath, saveTextToFile } = frodo.utils;
-const { readCustomNode, readCustomNodes } = frodo.authn.node;
+const { readCustomNode, readCustomNodes, importCustomNodes } = frodo.authn.node;
 
 /**
  * Export all custom nodes to 'custom-nodes/nodes' directory.
@@ -44,4 +45,56 @@ export async function configManagerExportCustomNodes(
     printError(error);
     return false;
   }
+}
+
+/**
+ * Import all custom nodes to specified tenant.
+ * @param {string} name Optional display name of a custom node to import. If not provided, all custom nodes will be exported.
+ * @returns {Promise<boolean>} True if export was successful
+ */
+
+export async function configManagerImportCustomNode(
+  nodeName?: string
+): Promise<boolean> {
+  try {
+    if (nodeName) {
+      const nodeDir = getFilePath(`custom-nodes/nodes/${nodeName}`);
+      const jsonFilePath = `${nodeDir}/${nodeName}.json`;
+      const scriptFilePath = `${nodeDir}/${nodeName}.js`;
+      const customNodeData = { nodeTypes: {} };
+
+      const importData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
+
+      if (fs.existsSync(scriptFilePath)) {
+        importData.script = fs.readFileSync(scriptFilePath, 'utf8');
+      }
+
+      customNodeData.nodeTypes[importData._id] = importData;
+      await importCustomNodes(undefined, nodeName, customNodeData);
+    } else {
+      const nodesDir = getFilePath(`custom-nodes/nodes`);
+      const nodeFolders = fs.readdirSync(nodesDir);
+      const customNodeData = { nodeTypes: {} };
+
+      for (const nodeFolder of nodeFolders) {
+        const jsonFilePath = `${nodesDir}/${nodeFolder}/${nodeFolder}.json`;
+        const scriptFilePath = `${nodesDir}/${nodeFolder}/${nodeFolder}.js`;
+
+        const importData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
+
+        if (fs.existsSync(scriptFilePath)) {
+          importData.script = fs.readFileSync(scriptFilePath, 'utf8');
+        }
+
+        customNodeData.nodeTypes[importData._id] = importData;
+
+        await importCustomNodes(undefined, nodeName, customNodeData);
+      }
+    }
+
+    return true;
+  } catch (error) {
+    printError(error, `Error importing custom nodes`);
+  }
+  return false;
 }
